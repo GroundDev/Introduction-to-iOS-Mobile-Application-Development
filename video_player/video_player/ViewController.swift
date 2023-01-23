80e7cbdb3bf5dfebdf11072d363a01a05dcb9e67
Ground <27danny@naver.com>
2023-01-23 15:22:54 +0900

    Create Project

---
+++ video_player/video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by KimJS on 2023/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
